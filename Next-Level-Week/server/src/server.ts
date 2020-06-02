0dd9cd847261350e5e06f9bb9728e6003818e795
t2m.gAbranches <guilherme.abranches@t2mlab.com>
2020-06-02 18:16:46 -0300

    2º dia completo & Back-End criado :+1: :rocket: :alien:

--- Next-Level-Week/server/src/server.ts
+++ Next-Level-Week/server/src/server.ts
@@ -1,17 +1,14 @@
 import express from 'express';
+import path from 'path';
+import routes from './routes';
+import cors from 'cors';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    console.log("Listagem de Usuários");
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
-
-
-    response.json([
-        'Diego',
-        'Guilherme',
-        'Paula'
-    ]);
-});
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
