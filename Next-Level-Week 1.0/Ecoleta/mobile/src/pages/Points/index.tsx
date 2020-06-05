d95df29777e7c82b5e71857310e8189212eb4795
t2m.gAbranches <guilherme.abranches@t2mlab.com>
2020-06-05 11:02:48 -0300

    4º dia finalizado (App Mobile - React-Native) :rocket: :green_heart: :+1:

---
+++ Next-Level-Week 1.0/Ecoleta/mobile/src/pages/Points/index.tsx
@@ -0,0 +1,279 @@
+import React, { useEffect, useState } from 'react';
+import { Feather as Icon } from '@expo/vector-icons';
+import { View, Text, ScrollView, Image, StyleSheet, SafeAreaView, TouchableOpacity, Alert } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import MapView, { Marker } from 'react-native-maps';
+import { SvgUri } from 'react-native-svg';
+import * as Location from 'expo-location';
+import api from '../../services/api';
+
+interface Item {
+    id: number;
+    title: string;
+    image_url: string;
+}
+
+interface Point {
+    id: number;
+    name: string;
+    image: string;
+    latitude: number;
+    longitude: number;
+}
+
+interface Params {
+    uf: string;
+    city: string;
+}
+
+const Points = () => {
+    const navigation = useNavigation();
+
+    const route = useRoute();
+
+    const routeParams = route.params as Params;
+
+
+    const [items, setItems] = useState<Item[]>([]);
+
+    const [points, setPoints] = useState<Point[]>([]);
+
+    const [selectedItems, setSelectedItems] = useState<number[]>([]);
+
+    const [inicialPosition, setInicialPosition] = useState<[number, number]>([0, 0]);
+
+    useEffect(() => {
+        async function loadPosition() {
+            const { status } = await Location.requestPermissionsAsync();
+
+            if (status !== 'granted') {
+                Alert.alert('Ooopss...', 'Precisamos de sua permissão para obter a sua localizção!')
+                return;
+            }
+
+            const location = await Location.getCurrentPositionAsync();
+
+            const { latitude, longitude } = location.coords;
+
+            setInicialPosition([
+                latitude,
+                longitude
+            ])
+        };
+
+        loadPosition();
+    }, []);
+
+    useEffect(() => {
+        api.get('items').then(response => {
+            setItems(response.data);
+        });
+    }, []);
+
+    useEffect(() => {
+        api.get('points', {
+            params: {
+                city: routeParams.city,
+                uf: routeParams.uf,
+                items: selectedItems
+            }
+        }).then(response => {
+            setPoints(response.data);
+        })
+    }, [selectedItems]);
+
+    function handleNavigationBack() {
+        navigation.goBack();
+    };
+
+    function handleNavigationToDetail(id: number) {
+        navigation.navigate('Detail', { point_id: id });
+    };
+
+    function handleSelectItem(id: number) {
+        const alreadySelected = selectedItems.findIndex(item => item === id);
+
+        if (alreadySelected >= 0) {
+            const filteredItems = selectedItems.filter(item => item !== id);
+
+            setSelectedItems(filteredItems);
+        } else {
+            setSelectedItems([...selectedItems, id]);
+        }
+    };
+
+    return (
+        <SafeAreaView style={{ flex: 1, paddingTop: 40 }}>
+            <View style={styles.container}>
+                <TouchableOpacity onPress={handleNavigationBack}>
+                    <Icon name="arrow-left" size={20} color="#34cb79" />
+                </TouchableOpacity>
+
+                <Text style={styles.title}>Bem-vindo.</Text>
+                <Text style={styles.description}>Encontre no mapa uma ponto de coleta.</Text>
+
+                <View style={styles.mapContainer}>
+                    {inicialPosition[0] !== 0 && (
+
+                        <MapView
+                            style={styles.map}
+                            loadingEnabled={inicialPosition[0] === 0}
+                            initialRegion={{
+                                latitude: inicialPosition[0],
+                                longitude: inicialPosition[1],
+                                latitudeDelta: 0.014,
+                                longitudeDelta: 0.014,
+                            }}>
+                            {points.map(point => (
+                                <Marker
+                                    key={String(point.id)}
+                                    onPress={() => handleNavigationToDetail(point.id)}
+                                    style={styles.mapMarker}
+                                    coordinate={{
+                                        latitude: point.latitude,
+                                        longitude: point.longitude,
+                                    }}
+                                >
+                                    <View style={styles.mapMarkerContainer}>
+                                        <Image style={styles.mapMarkerImage} source={{ uri: point.image }} />
+                                        <Text style={styles.mapMarkerTitle}>{point.name}</Text>
+                                    </View>
+                                </Marker>
+                            ))}
+                        </MapView>
+
+                    )}
+
+                </View>
+            </View>
+            <View style={styles.itemsContainer}>
+                <ScrollView
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    contentContainerStyle={{
+                        paddingHorizontal: 24
+                    }}
+                >
+                    {items.map(item => (
+                        <TouchableOpacity
+                            key={String(item.id)}
+                            style={[
+                                styles.item,
+                                selectedItems.includes(item.id) ? styles.selectedItem : {}
+                            ]}
+                            onPress={() => handleSelectItem(item.id)}
+                            activeOpacity={0.6}
+                        >
+                            <SvgUri
+                                width={42}
+                                height={42}
+                                uri={item.image_url}
+                            />
+                            <Text style={styles.itemTitle}>{item.title}</Text>
+                        </TouchableOpacity>
+                    ))}
+                </ScrollView>
+            </View>
+        </SafeAreaView>
+    );
+};
+
+export default Points;
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingHorizontal: 32,
+        //paddingTop: 20 + Constants.statusBarHeight,
+    },
+
+    title: {
+        fontSize: 20,
+        fontFamily: 'Ubuntu_700Bold',
+        marginTop: 24,
+    },
+
+    description: {
+        color: '#6C6C80',
+        fontSize: 16,
+        marginTop: 4,
+        fontFamily: 'Roboto_400Regular',
+    },
+
+    mapContainer: {
+        flex: 1,
+        width: '100%',
+        borderRadius: 10,
+        overflow: 'hidden',
+        marginTop: 16,
+    },
+
+    map: {
+        width: '100%',
+        height: '100%',
+    },
+
+    mapMarker: {
+        width: 90,
+        height: 80,
+    },
+
+    mapMarkerContainer: {
+        width: 90,
+        height: 70,
+        backgroundColor: '#34CB79',
+        flexDirection: 'column',
+        borderRadius: 8,
+        overflow: 'hidden',
+        alignItems: 'center'
+    },
+
+    mapMarkerImage: {
+        width: 90,
+        height: 45,
+        resizeMode: 'cover',
+    },
+
+    mapMarkerTitle: {
+        flex: 1,
+        fontFamily: 'Roboto_400Regular',
+        color: '#FFF',
+        fontSize: 13,
+        lineHeight: 23,
+    },
+
+    itemsContainer: {
+        flexDirection: 'row',
+        marginTop: 16,
+        marginBottom: 32,
+    },
+
+    item: {
+        backgroundColor: '#fff',
+        borderWidth: 2,
+        borderColor: '#eee',
+        height: 120,
+        width: 120,
+        borderRadius: 8,
+        paddingHorizontal: 16,
+        paddingTop: 20,
+        paddingBottom: 8,
+        marginRight: 8,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+
+        textAlign: 'center',
+    },
+
+    selectedItem: {
+        borderColor: '#34CB79',
+        borderWidth: 2,
+    },
+
+    itemTitle: {
+        fontFamily: 'Roboto_400Regular',
+        textAlign: 'center',
+        fontSize: 13,
+    },
+});
